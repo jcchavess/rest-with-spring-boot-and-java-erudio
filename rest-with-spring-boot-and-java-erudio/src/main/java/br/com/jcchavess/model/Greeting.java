0f728369cd67a6026ee1c5d1bd6eb6dec879f477
jcchavess <joaocarlos_chavesfilho@hotmail.com>
2025-02-27 23:51:11 -0300

    Firsts steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jcchavess/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.jcchavess.model;
+
+public record Greeting(long id, String content) {
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/jcchavess/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.jcchavess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
