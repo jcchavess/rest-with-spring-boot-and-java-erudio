1e032a146483f7f959e98f86b36e4fcb253bb0dc
jcchavess <joaocarlos_chavesfilho@hotmail.com>
2025-02-28 23:20:58 -0300

    Segundo dia de estudos.

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jcchavess/request/converters/NumberConverter.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jcchavess/request/converters/NumberConverter.java
@@ -1,4 +1,18 @@
 package br.com.jcchavess.request.converters;
 
 public class NumberConverter {
+
+    public static Double convertToDouble(String strNumber) throws IllegalArgumentException {
+
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber) {
+
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
+    }
 }
